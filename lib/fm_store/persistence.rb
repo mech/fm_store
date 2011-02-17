589ce1c91f129c6a5290fc4c1748ed9b69bbbb3d
mech <mech@me.com>
2011-02-17 12:18:00 +0800

    Clear out empty value for date/time

--- lib/fm_store/persistence.rb
+++ lib/fm_store/persistence.rb
@@ -2,42 +2,54 @@
 module FmStore
   module Persistence
     extend ActiveSupport::Concern
-    
+
     # The place where all the persistence took place, like insert, update
     module ClassMethods
       def create(attributes = {})
-        
+
       end
     end
-    
+
     # Instance methods
     def save
       create_or_update
     end
-    
+
     def update_attributes(attributes = {})
       if valid?
         attrs = {}
-        
+
         attributes.each do |field, value|
           field = field.to_s
-          
+
           fm_name = self.class.find_fm_name(field)
           type = self.class.find_fm_type(field)
-          
+
           if fm_name
             if type == Date
-              value = value.strftime("%m/%d/%Y") if value
+              if value.blank?
+                value = '' # clear the date
+              else
+                value = value.strftime("%m/%d/%Y")
+              end
             elsif type == DateTime
-              value = value.strftime("%m/%d/%Y %H:%M:%S") if value
+              if value.blank?
+                value = ''
+              else
+                value = value.strftime("%m/%d/%Y %H:%M:%S")
+              end
             elsif type == Time
-              value = value.strftime("%H:%M") if value
+              if value.blank?
+                value = ''
+              else
+                value = value.strftime("%H:%M")
+              end
             end
-            
+
             attrs[fm_name] = value
           end
         end
-        
+
         run_callbacks(:save) do
           conn = Connection.establish_connection(self.class)
           result = conn.edit(@record_id, attrs)
@@ -46,7 +58,7 @@ module FmStore
         false
       end
     end
-    
+
     # Throws Rfm::Error::RecordAccessDeniedError if no permission to delete
     def destroy
       run_callbacks(:destroy) do
@@ -56,22 +68,22 @@ module FmStore
         end
       end
     end
-    
+
     alias :delete :destroy
-    
+
     protected
-    
+
     # Will always return +self+
     def create_or_update
       result = new_record? ? create : update
     end
-    
+
     def create
       if valid?
         run_callbacks(:save) do
           conn = Connection.establish_connection(self.class)
           result = conn.create(self.fm_attributes)
-          
+
           @record_id = result[0].record_id
           @new_record = false
         end; self
@@ -79,7 +91,7 @@ module FmStore
         false
       end
     end
-    
+
     def update
       if valid?
         run_callbacks(:save) do
@@ -90,6 +102,6 @@ module FmStore
         false
       end
     end
-     
+
   end
 end
