45b23865c9668276e50671e9dd62d893c2c3e96e
mech <mech@me.com>
2011-04-02 20:43:59 +0800

    JSON and XML Serializers

--- lib/fm_store/components.rb
+++ lib/fm_store/components.rb
@@ -2,13 +2,15 @@
 module FmStore
   module Components
     extend ActiveSupport::Concern
-    
+
     included do
       include FmStore::Fields
       include FmStore::Associations
       include FmStore::Persistence
       include ActiveModel::Validations
-      
+      include ActiveModel::Serializers::JSON
+      include ActiveModel::Serializers::Xml
+
       extend ActiveModel::Callbacks
       extend ActiveModel::Naming
       extend FmStore::Finders
--- lib/fm_store/layout.rb
+++ lib/fm_store/layout.rb
@@ -2,33 +2,33 @@
 module FmStore
   module Layout
     extend ActiveSupport::Concern
-    
+
     included do
       include FmStore::Components
-      
+
       cattr_accessor :layout, :database
-      
+
       attr_reader :new_record, :record_id, :mod_id
-      
+
       define_model_callbacks :create, :save, :update, :validation, :destroy
     end
-    
+
     module ClassMethods
       def set_layout(layout)
         self.layout = layout
       end
-      
+
       def set_database(database)
         self.database = database
       end
-      
+
       # Calling self.fields will ideally match here
       # See FieldControl
       def fm_fields
         conn = Connection.establish_connection(self)
         rs = conn.any.first.keys.inspect
       end
-      
+
       # Return the real FileMaker, nil otherwise
       def find_fm_name(attribute_name)
         if fields.has_key?(attribute_name)
@@ -39,47 +39,47 @@ module FmStore
           f.last.fm_name if f
         end
       end
-      
+
       def find_fm_type(attribute_name)
         f = fields.find { |a| a.last.name == attribute_name }
-        
+
         f.last.type if f
       end
-      
+
       def searchable_fields
         fields.map(&:last).select(&:searchable).map(&:name)
       end
-      
+
       def identity
         fields.map(&:last).find(&:identity).try(:fm_name) || "-recid"
       end
-      
+
       # Drop-down, for example
       # http://host/fmi/xml/FMPXMLLAYOUT.xml?-db=jobs+&-lay=jobs&-view=
       def value_lists
         conn = Connection.establish_connection(self)
         conn.value_lists
       end
-      
+
       def first
         limit(1).first
       end
     end
-    
+
     def initialize(attributes = {})
       @associations = {}
       @new_record = true
       process(attributes)
     end
-    
+
     def fm_attributes
-      attrs = {}
-      
+      @attributes = {}
+
       fields.each do |fm_attr, field|
         ivar = send("#{field.name}")
-        
+
         type = field.type
-        
+
         if type == Date
           ivar = ivar.strftime("%m/%d/%Y") if ivar
         elsif type == DateTime
@@ -87,7 +87,7 @@ module FmStore
         elsif type == Time
           ivar = ivar.strftime("%H:%M") if ivar
         end
-        
+
         # case ivar
         # when Date
         #   ivar = ivar.strftime("%m/%d/%Y")
@@ -96,45 +96,49 @@ module FmStore
         # when Time
         #   ivar = ivar.strftime("%H:%M")
         # end
-        
-        attrs[fm_attr] = ivar if ivar # ignore nil attributes
+
+        @attributes[fm_attr] = ivar if ivar # ignore nil attributes
       end
-      
-      attrs
+
+      @attributes
     end
-    
+
+    def attributes
+      @attributes
+    end
+
     def reload
       @associations = {}
       self.class.id(id)
     end
-    
+
     def id
       self.class.identity == "-recid" ? @record_id : send(self.class.fields[self.class.identity].name)
     end
-    
+
     def new_record?
       @new_record
     end
-    
+
     def to_param
       id.to_s if id
     end
-    
+
     # Require by ActiveModel
     def to_model
       self
     end
-    
+
     def to_key
       id if id
     end
-    
+
     def persisted?
       !new_record?
     end
-    
+
     protected
-    
+
     def process(attributes)
       attributes.each do |k, v|
         send("#{k}=", v) if respond_to?(k)
