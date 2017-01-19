30e9d8e88a099780fea40fb4e40136bf7efd0821
mech <mech@me.com>
2017-01-19 10:11:58 +0800

    Remove dollar sign and decimal mark

--- lib/fm_store/ext/field.rb
+++ lib/fm_store/ext/field.rb
@@ -1,11 +1,15 @@
 module Rfm
   module Metadata
     class Field
+      def remove_decimal_mark(value)
+        value.delete(',')
+      end
+
       def coerce(value, resultset)
         return nil if (value.nil? || value.empty?)
         case self.result
         when "text"      then value
-        when "number"    then BigDecimal.new(value)
+        when "number"    then BigDecimal.new(remove_decimal_mark(value))
         when "date"      then Date.strptime(value, resultset.date_format)
         when "time"      then DateTime.strptime("1/1/-4712 #{value}", "%m/%d/%Y #{resultset.time_format}")
         when "timestamp" then DateTime.strptime(value, resultset.timestamp_format)
