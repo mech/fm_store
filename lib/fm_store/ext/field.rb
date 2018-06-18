7fd67e09b608fb6f238b0c8902b54d15d385f299
mech <mech@me.com>
2018-06-18 12:05:53 +0800

    BigDecimal.new is deprecated

--- lib/fm_store/ext/field.rb
+++ lib/fm_store/ext/field.rb
@@ -10,7 +10,7 @@ module Rfm
         return nil if (value.nil? || value.empty?)
         case self.result
         when "text"      then value
-        when "number"    then BigDecimal.new(remove_decimal_mark(value))
+        when "number"    then BigDecimal(remove_decimal_mark(value))
         when "date"      then Date.strptime(value, resultset.date_format)
         when "time"      then DateTime.strptime("1/1/-4712 #{value}", "%m/%d/%Y #{resultset.time_format}")
         when "timestamp" then DateTime.strptime(value, resultset.timestamp_format)
