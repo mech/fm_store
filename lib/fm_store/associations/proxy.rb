c7249d17cb3f80ea1bb8bf830df3789f29fdf6c8
mech <mech@me.com>
2019-08-06 10:13:58 +0800

    Re-implement nil? for target

--- lib/fm_store/associations/proxy.rb
+++ lib/fm_store/associations/proxy.rb
@@ -5,13 +5,17 @@ module FmStore #:nodoc
       instance_methods.each do |method|
         undef_method(method) unless method =~ /(^__|^nil\?$|^send$|^object_id$|^extend$)/
       end
-      
+
       attr_reader :target, :options
-      
+
+      # Somehow nil? will not be recognized for Proxy target
+      def nil?
+        @target == nil
+      end
+
       def method_missing(name, *args, &block)
         @target.send(name, *args, &block)
       end
-      
     end
   end
 end
